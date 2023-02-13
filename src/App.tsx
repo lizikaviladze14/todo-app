5a9f234e389f2081450fada75b736740867f7058
lizikaviladze14 <lizikavila@gmail.com>
2023-02-13 18:13:28 +0400

    json-server

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
         const fetchData = async () => {
             setIsLoading(true);
             try {
-                const response = await fetch('http://localhost:3000/todos');
+                const response = await fetch('http://localhost:3005/todos');
                 const todos = await response.json();
                 setTodos(todos);
             } catch (err) {
@@ -29,7 +29,7 @@ function App() {
     const addTodo = async (todoTitle: string) => {
         if(!todoTitle) return
         try {
-            const response = await fetch('http://localhost:3000/todos', {
+            const response = await fetch('http://localhost:3005/todos', {
                 method: 'POST',
                 body: JSON.stringify({
                     title: todoTitle,
@@ -47,7 +47,7 @@ function App() {
     }
 
     const toggleTodoCompletedById = (todoId: string, isCompleted: boolean) => {
-        fetch(`http://localhost:3000/todos/${todoId}`, {
+        fetch(`http://localhost:3005/todos/${todoId}`, {
             method: 'PATCH',
             body: JSON.stringify({
                 isCompleted: isCompleted
@@ -71,7 +71,7 @@ function App() {
 
     const deleteTodoItem = async (todoId: string) => {
         try {
-            await fetch(`http://localhost:3000/todos/${todoId}`, {
+            await fetch(`http://localhost:3005/todos/${todoId}`, {
                 method: 'DELETE',
             });
             setTodos(todos.filter(todo => todo.id !== todoId))
